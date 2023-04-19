1c84ff6da872532da70a61c6eaabf151f2bcc2b5
CloudHolic <bluegkstjr@kaist.ac.kr>
2023-04-19 15:40:32 +0900

    Add project files.

---
+++ Sharenian/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sharenian;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App
+{
+    public App() => Activated += StartElmish;
+
+    private void StartElmish(object? sender, EventArgs e)
+    {
+        Activated -= StartElmish;
+        Program.main(MainWindow);
+    }
+}
