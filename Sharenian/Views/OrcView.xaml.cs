f15f25bba533fd1e2c1bc18ddeedc8fde46ef2ac
CloudHolic <bluegkstjr@kaist.ac.kr>
2024-01-02 07:30:22 +0900

    Add Ocrview

---
+++ Sharenian/Views/OrcView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sharenian.Views;
+/// <summary>
+/// Interaction logic for OrcView.xaml
+/// </summary>
+public partial class OrcView : UserControl
+{
+    public OrcView()
+    {
+        InitializeComponent();
+    }
+}
